23e5d729ad374e96ee8aa0be4471c2d47815b613
unknown <brcamille@gmail.com>
2025-04-22 21:11:24 +0200

    fix : linter

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -3,7 +3,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("dummy_api_key");
 
 export async function POST(request: Request) {
   try {
